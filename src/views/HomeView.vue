4cb1a7da78673633d349ef425ef91babe4cdf9ef
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-27 08:32:12 +0530

    buil syntex change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello this is home page</h1>
 </template>
